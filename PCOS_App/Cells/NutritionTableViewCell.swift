f220a25021a7a606da44c89850f1e3a64810138e
Pradeep Biswas <pradeep211005@gmail.com>
2025-12-03 17:44:58 +0530

    Worked on nutrition card

--- PCOS_App/Cells/NutritionTableViewCell.swift
+++ PCOS_App/Cells/NutritionTableViewCell.swift
@@ -9,8 +9,15 @@ import UIKit
 
 class NutritionTableViewCell: UITableViewCell {
 
-    @IBOutlet weak var nutritionCell: UIView!
+    @IBOutlet weak var carbsGram: UILabel!
     
+    @IBOutlet weak var fatsGram: UILabel!
+    @IBOutlet weak var proteinGram: UILabel!
+    @IBOutlet weak var fatsProgress: UIProgressView!
+    @IBOutlet weak var carbsProgress: UIProgressView!
+    @IBOutlet weak var proteinProgress: UIProgressView!
+    @IBOutlet weak var caloriesConsumed: UILabel!
+    @IBOutlet weak var completionCircle: UIImageView!
     static let identifier = "NutritionTableViewCell"
     static func nib() -> UINib {
         return UINib(nibName: "NutritionTableViewCell", bundle: nil)
--- PCOS_App/Controller/DietViewController.swift
+++ PCOS_App/Controller/DietViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class DietViewController: UIViewController {
-    
+
     var dummyData = DataStore.sampleFoods
     @IBOutlet weak var tableView: UITableView!
     
@@ -19,10 +19,8 @@ class DietViewController: UIViewController {
         let calendar = UIBarButtonItem(image: UIImage(systemName: "calendar"), style: .plain, target: self, action: #selector(calendarTapped))
         navigationItem.rightBarButtonItem = calendar
         
-        
-        let nib = UINib(nibName: "LogsTableViewCell", bundle: nil)
-        tableView.register(LogsTableViewCell.nib(), forCellReuseIdentifier: "LogsTableViewCell")
-        tableView.register(NutritionTableViewCell.nib(), forCellReuseIdentifier: "NutritionTableViewCell")
+        tableView.register(LogsTableViewCell.nib(), forCellReuseIdentifier: LogsTableViewCell.identifier)
+        tableView.register(NutritionTableViewCell.nib(), forCellReuseIdentifier: NutritionTableViewCell.identifier)
         tableView.dataSource = self
         tableView.delegate = self
     }
@@ -34,30 +32,38 @@ class DietViewController: UIViewController {
     }
 }
 
-
-
-
-
 extension DietViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return dummyData.count
+        
+        return dummyData.count + 1
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-//        if indexPath.row == 0{
-//            let cell = tableView.dequeueReusableCell(withIdentifier: "NutritionTableViewCell") as! NutritionTableViewCell
-//            return cell
-//        }
-        let cell = tableView.dequeueReusableCell(withIdentifier: "LogsTableViewCell", for: indexPath) as! LogsTableViewCell
-        cell.foodName.text = dummyData[indexPath.row].name
-        cell.calories.text = "\(dummyData[indexPath.row].calories)kcal"
+
+        if indexPath.row == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: NutritionTableViewCell.identifier) as! NutritionTableViewCell
+            return cell
+        }
+        
+  
+        let cell = tableView.dequeueReusableCell(withIdentifier: LogsTableViewCell.identifier, for: indexPath) as! LogsTableViewCell
+        let dataIndex = indexPath.row - 1
+        let item = dummyData[dataIndex]
+        cell.foodName.text = item.name
+        cell.calories.text = "\(item.calories)kcal"
         cell.foodName.font = UIFont.systemFont(ofSize: 18, weight: .bold)
-        cell.carbs.text = "\(dummyData[indexPath.row].carbsContent)g"
-        cell.protein.text = "\(dummyData[indexPath.row].proteinContent)g"
-        cell.fats.text = "\(dummyData[indexPath.row].fatsContent)g"
+        cell.carbs.text = "\(item.carbsContent)g"
+        cell.protein.text = "\(item.proteinContent)g"
+        cell.fats.text = "\(item.fatsContent)g"
         cell.imageView?.image = UIImage(named: "biryani")
         cell.imageView?.layer.cornerRadius = 10
         return cell
     }
-        
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.row == 0 {
+            return 250
+        }
+        return 100
+    }
 }
