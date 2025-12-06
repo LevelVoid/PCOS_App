4117f77158a61df233f5799d8e4a40bf21ed51c6
Pradeep Biswas <pradeep211005@gmail.com>
2025-12-06 16:47:40 +0530

    Completed main diet screen and organized the project structure

--- PCOS_App/Diet/Cells/NutritionHeader.swift
+++ PCOS_App/Diet/Cells/NutritionHeader.swift
@@ -9,6 +9,36 @@ import UIKit
 
 class NutritionHeader: UIView {
 
+    @IBOutlet weak var nutritionCard: UIView!
+    @IBOutlet weak var recommendationCard: UIView!
     
-
+    @IBOutlet weak var stackMacros: UIStackView!
+    @IBOutlet weak var progressCircle: UIImageView!
+    @IBOutlet weak var fatsProgress: UIProgressView!
+    @IBOutlet weak var carbsProgress: UIProgressView!
+    @IBOutlet weak var proteinProgress: UIProgressView!
+    
+    @IBOutlet weak var calToBeConsumed: UILabel!
+    @IBOutlet weak var caloriesConsumed: UILabel!
+    
+    @IBOutlet weak var fatsGm: UILabel!
+    @IBOutlet weak var carbsGm: UIView!
+    @IBOutlet weak var proteinGm: UILabel!
+    
+    func configure(){
+        nutritionCard.layer.cornerRadius = 25
+        recommendationCard.layer.cornerRadius = 15
+        
+        nutritionCard.layer.borderWidth = 1
+        nutritionCard.layer.borderColor = UIColor.systemGray4.cgColor
+        
+        recommendationCard.layer.shadowColor = UIColor.black.cgColor
+        recommendationCard.layer.shadowOpacity = 0.2
+        
+        stackMacros.layer.cornerRadius = 25
+        stackMacros.layer.backgroundColor = UIColor.systemGray5.cgColor
+        
+        caloriesConsumed.text = "1000"
+        
+    }
 }
--- PCOS_App/Diet/Controller/DietViewController.swift
+++ PCOS_App/Diet/Controller/DietViewController.swift
@@ -21,9 +21,9 @@ class DietViewController: UIViewController {
         
         let header = Bundle.main.loadNibNamed("NutritionHeader", owner: self, options: nil)?.first as! NutritionHeader
         tableView.tableHeaderView = header
-        header.frame.size.height = 200
+        header.configure()
+        header.frame.size.height = 380
         tableView.register(LogsTableViewCell.nib(), forCellReuseIdentifier: LogsTableViewCell.identifier)
-        tableView.register(NutritionTableViewCell.nib(), forCellReuseIdentifier: NutritionTableViewCell.identifier)
         tableView.dataSource = self
         tableView.delegate = self
     }
@@ -38,40 +38,23 @@ class DietViewController: UIViewController {
 extension DietViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
-        return dummyData.count + 1
+        return dummyData.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 
-        if indexPath.row == 0 {
-            let cell = tableView.dequeueReusableCell(withIdentifier: NutritionTableViewCell.identifier) as! NutritionTableViewCell
-            return cell
-        }
-        
-  
         let cell = tableView.dequeueReusableCell(withIdentifier: LogsTableViewCell.identifier, for: indexPath) as! LogsTableViewCell
-        let dataIndex = indexPath.row - 1
+        let dataIndex = indexPath.row
         let item = dummyData[dataIndex]
         cell.configure(with: item)
-        
-//        cell.foodName.text = item.name
-//        cell.calories.text = "\(Int(item.calories))kcal"
-//        cell.foodName.font = UIFont.systemFont(ofSize: 15, weight: .bold)
-//        cell.carbs.text = "\(Int(item.carbsContent))g"
-//        cell.protein.text = "\(Int(item.proteinContent))g"
-//        cell.fats.text = "\(Int(item.fatsContent))g"
-//        
-//        cell.foodImg.image = UIImage(named: "biryani")
-//        cell.foodImg.layer.cornerRadius = 10
-//        cell.foodImg.clipsToBounds = true
-        
         return cell
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        if indexPath.row == 0 {
-            return 250
-        }
         return 100
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        "Today's Logs"
     }
 }
